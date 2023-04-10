b374018b3924c86ea0c41e984471c35570757af3
RonaldQueirox <121915325+RonaldQueirox@users.noreply.github.com>
2023-04-10 20:04:13 -0300

    Update examples

---
+++ exercicíos/ex081.py
@@ -0,0 +1,28 @@
+# Desafio 094
+
+gal = list()
+pessoa = dict()
+soma = média = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    gal.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'A) Ao temos {len(gal)} pessoas cadastradas.')
+média = soma / len(gal)
+print(f'B) A média de idade é de {média:5.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
